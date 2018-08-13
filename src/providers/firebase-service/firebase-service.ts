aa487b542cd418453a6f1cf2d050a1700f312036
Hussain Abbas <hussain.jazing.21@gmail.com>
2018-08-13 11:24:11 +0300

    fix uncompeted push at Hussain Abbas  tasks

--- src/providers/firebase-service/firebase-service.ts
+++ src/providers/firebase-service/firebase-service.ts
@@ -72,7 +72,7 @@ export class authFirebaseService {
       return this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
       .then(loginUser => {
         localStorage.setItem('uid', loginUser.user.uid)
-        this.setUserInfoLocalStorage()
+        this.setUserInfoLocalStorage(authData)
     })
 
     }
@@ -83,7 +83,7 @@ export class authFirebaseService {
         return this.afAuth.auth.sendPasswordResetEmail(email);
         }
     // ============================ Set user data at the local storage ===========================
-    setUserInfoLocalStorage(){
+    setUserInfoLocalStorage(authData){
       let usersListObject = this.db.object('userData')
 
       usersListObject.snapshotChanges().subscribe(action=>{
@@ -95,7 +95,8 @@ export class authFirebaseService {
 
             for (const item of this.myObject) {
               if (item[1]['uid'] == localStorage.getItem('uid')) {
-                localStorage.setItem("userData", JSON.stringify(item[1]));
+                let userInfoData = [authData, item[1], item[0]]
+                localStorage.setItem("userData", JSON.stringify(userInfoData))
                 this._Events.publish("auth:Success")
                 localStorage.setItem('isLogin','true')
               }
@@ -105,46 +106,35 @@ export class authFirebaseService {
       })
     }
 
+
     // ============================ Regester Function ===========================
     regesterWithEmail(authData, userData){
-        return this.afAuth.auth.createUserWithEmailAndPassword(authData.email,authData.password)
-        .then(user=>{
-        
-
-        // ========================================================================
-        // ========== Setting the user information in the local storage ===========
-        // ========================================================================
-        localStorage.setItem('isLogin','true')
-        localStorage.setItem("userData", JSON.stringify(userData));
+      return this.afAuth.auth.createUserWithEmailAndPassword(authData.email,authData.password)
+      .then(user=>{
         localStorage.setItem('uid', this.afAuth.auth.currentUser.uid)
         userData.uid = localStorage.getItem('uid')
-        // ========================================================================
-        // ========== Setting the user information in the local storage ===========
-        // ========================================================================
-
-
-        
-        // ========================================================================
-        // ====================== User Profile Details ============================
-        // ========================================================================
-        this.usersList.push({
-            uid: userData.uid,
-            name: userData.name,
-            province: userData.province,
-            zone: userData.zone,
-            userType: userData.userType,
-            pharmacyReplyNo: userData.pharmacyReplyNo,
-            pharmacyAdress: userData.pharmacyAdress
-        }).then(()=>{
-          this._Events.publish("auth:Success")
-        })
-        // ========================================================================
-        // ====================== User Profile Details ============================
-        // ========================================================================
-        }).catch(error=>{
-        console.error(error)
-        })
-    }
+      // ========================================================================
+      // ====================== User Profile Details ============================
+      // ========================================================================
+      this.usersList.push({
+          uid: userData.uid,
+          name: userData.name,
+          province: userData.province,
+          zone: userData.zone,
+          userType: userData.userType,
+          pharmacyReplyNo: userData.pharmacyReplyNo,
+          pharmacyAdress: userData.pharmacyAdress
+      }).then((posta)=>{
+        console.log(posta);
+        this.setUserInfoLocalStorage(authData)
+      })
+      // ========================================================================
+      // ====================== User Profile Details ============================
+      // ========================================================================
+      }).catch(error=>{
+      console.error(error)
+      })
+  }
 
     logOut(){
       this.afAuth.auth.signOut()
