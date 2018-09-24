0463384830a5b6f69e53c72d05300da5bb573de5
hala hazim <hala5hazem@gmail.com>
2018-09-25 00:50:55 +0300

    testing

--- src/providers/firebase-service/firebase-service.ts
+++ src/providers/firebase-service/firebase-service.ts
@@ -24,8 +24,7 @@ export class postsFirebaseService {
 
  
   addPosts(myList:postModel) {
-    return this.dataList.push(myList)
-    
+    return this.dataList.push(myList) 
     this._OneSignal.postNotification({
       app_id:"e2304606-4ab1-4f9d-a0ea-1c83518b62af",
       included_segments: ['pharmacySegment'],
