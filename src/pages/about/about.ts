a60acac71f0703a81d1661c7c9f467792d44c596
hala hazim <hala5hazem@gmail.com>
2019-01-06 00:29:11 +0300

    edit css about page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -25,54 +25,65 @@ export class AboutPage {
   showinfo(){
    
       const alert = this.alertCtrl.create({
-        title: '<h2>حسين عباس<h2>',
-        subTitle: '<h3>صاحب الفكرة <h3> <h3> شارك في البرمجة والتصميم<h3>  ',
-        buttons: ['OK']
+        title: '<h1>حسين عباس<h1>',
+        message: '<h3>صاحب الفكرة <h3> <h3> شارك في البرمجة والتصميم<h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
     showinfo1(){
    
       const alert = this.alertCtrl.create({
-        title: '<h2>حسين كريم<h2>',
-        subTitle: ' <h3> شارك في البرمجة <h3>  ',
-        buttons: ['OK']
+        title: '<h1>حسين كريم<h1>',
+        message: ' <h3> شارك في البرمجة <h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
-    showinfo2(){
-   
+    showinfo2(){        
       const alert = this.alertCtrl.create({
-        title: '<h2>حلا حازم<h2>',
-        subTitle: ' <h3> شاركت في البرمجة والخوارزمية<h3>  ',
-        buttons: ['OK']
+        title: '<h1>حلا حازم<h1>',
+        message: ' <h3> شاركت في البرمجة والخوارزمية<h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
     showinfo3(){
    
       const alert = this.alertCtrl.create({
-        title: '<h2>سيف علي<h2>',
-        subTitle: '<h3> شارك في البرمجة والشعار<h3>  ',
-        buttons: ['OK']
+        title: '<h1>سيف علي<h1>',
+        message: '<h3> شارك في البرمجة والشعار<h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
     showinfo4(){
    
       const alert = this.alertCtrl.create({
-        title: '<h2>الهام علي<h2>',
-        subTitle: ' <h3> شاركت في البرمجة <h3>  ',
-        buttons: ['OK']
+        title: '<h1>الهام علي<h1>',
+        message: ' <h3> شاركت في البرمجة <h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
     showinfo5(){
    
       const alert = this.alertCtrl.create({
-        title: '<h2>هبة ستار<h2>',
-        subTitle: ' <h3>المشرفة على التطبيق <h3>  ',
-        buttons: ['OK']
+        title: '<h1>هبة ستار<h1>',
+        message: ' <h3>المشرفة على التطبيق <h3>  ',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+
       });
       alert.present();
     }
